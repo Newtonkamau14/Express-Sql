557bd9bc168408dc45856fa7f6425c6e08ce8af0
Newtonkamau14 <newtonkamau14@gmail.com>
2023-01-12 23:59:33 +0300

    db

--- database/person.sql
+++ database/person.sql
@@ -13,3 +13,31 @@ CREATE TABLE Persons (
 );
 
 SHOW TABLES;
+
+
+INSERT INTO Persons 
+            (lastname, firstname, address, city)
+        VALUES
+            ('Anders','Maria','WA1 1DP','London'),
+            ('Hardy', 'Thomas', '12209','Berlin'),
+            ('Moos','Hanna','68306','Mannheim'),
+            ('Lincoln','Elizabeth','T2F 8M4','Tsawassen'),
+           /('Roulet','Annette','31000','Toulouse'),
+            ('Bertand','MArie','75012','Paris'),
+            ('Pipps','Georg','5020','Salzburg'),
+            ('Castro','ISabel','1756','Lisboa'),
+            ('Carvalho','Lucia','05487-020','Sao Paulo'),
+            ('Kloss','Horst','01307','Cunewalde'),
+            ('Holz','Micheal','1203','Geneve'),
+            ('Limeria','Jnete','02389-890','Riode Janiero'),
+            ('Moroni','Maurizio','42100','Reggio'),
+            ('Kunar','Hari','OX15 4NB','London'),
+            ('Jytte','Petersen','1934','Kobenhavn'),
+            ('Ibsen','','',''),
+            ('','','',''),
+            ('','','',''),
+            ('','','',''),
+
+
+        
+
